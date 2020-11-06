4c7bbf1d750cf7349afbd6796ec34959bda34fe1
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2020-11-05 19:50:20 -0500

    ASSIGNMENT #1

---
+++ Basic JavaScript/JS/main.js
@@ -0,0 +1,6 @@
+window.alert("Hello, Shiela!"); //defines the pop out text on the beginning
+document.write('Welcome!');    //defines to display a text 
+var green = "Shiela!";         //defining a variable and giving it string value
+var green = green.fontcolor("green");   //using the fontcolor on green variable
+document.write(green);          //defines the string 
+document.write(15+10);               //defines the expression
