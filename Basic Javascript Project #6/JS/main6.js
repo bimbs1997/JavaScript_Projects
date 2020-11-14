37de8d6a188877d7ab34364727123c4f1c3a0ce6
bimbs1997 <72235151+bimbs1997@users.noreply.github.com>
2020-11-14 13:20:29 -0500

    updates project#6

--- Basic Javascript Project #6/JS/main6.js
+++ Basic Javascript Project #6/JS/main6.js
@@ -1,18 +1,18 @@
-function Verify_function() {
+function Verify_function() {        //defines the ternary operation using input from the browser
     var Age, Can_verify;
     Age = document.getElementById("Age").value;
     Can_verify = (Age < 20 ) ? "You are too young to ":"You are old enough";
     document.getElementById("verify").innerHTML = Can_verify + " to enter this site THANK YOU!";
 }
 
-function vehicle (Make, Model, Year, Color) {
+function vehicle (Make, Model, Year, Color) {       //defines the this keyword fuction.
     this.vehicle_make = Make;
     this.vehicle_model = Model;
     this.vehicle_year = Year;
     this.vehicle_color = Color;
 }
 
-var Blake = new vehicle ("Ferrari", "Italia", 2020, "Red");
+var Blake = new vehicle ("Ferrari", "Italia", 2020, "Red");         //defines the new keyword and fuction.
 var Emerson = new vehicle ("Nissan", "370z", 2018, "Matte Black");
 var Shiela = new vehicle ("Nissan", "GT-R", 2020, "Glossy Silver");
 function owner_function() {
